704a0364979d9f3e7446e6995fb24bcb78c8001c
songfish <songyu_727@163.com>
2019-07-10 19:38:47 +0800

    add printtree

--- 从上到下打印二叉树.py
+++ 从上到下打印二叉树.py
@@ -4,7 +4,39 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
+
+
 class Solution:
     # 返回从上到下每个节点值列表，例：[1,2,3]
     def PrintFromTopToBottom(self, root):
         # write code here
+        if root is None:
+            return []
+        queue = []
+        result = []
+        queue.append(root)
+        while(len(queue)>0):
+            currentroot = queue.pop(0)
+            result.append(currentroot.val)
+            if currentroot.left:
+                queue.append(currentroot.left)
+            if currentroot.right:
+                queue.append(currentroot.right)
+        return result
+
+
+pNode1 = TreeNode(8)
+pNode2 = TreeNode(6)
+pNode3 = TreeNode(10)
+pNode4 = TreeNode(5)
+pNode5 = TreeNode(7)
+pNode6 = TreeNode(9)
+pNode7 = TreeNode(11)
+pNode1.left = pNode2
+pNode1.right = pNode3
+pNode2.left = pNode4
+pNode2.right = pNode5
+pNode3.left = pNode6
+pNode3.right = pNode7
+s = Solution()
+print(s.PrintFromTopToBottom(pNode1))
