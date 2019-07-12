ba4975cc34e23c87ebb59dd605c3c6ef125db9e3
songfish <songyu_727@163.com>
2019-07-12 14:55:37 +0800

    add solution

---
+++ 二叉树中和为某一值的路径.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    # 返回二维列表，内部每个列表表示找到的路径
+    def FindPath(self, root, expectNumber):
+        # write code here
+        if not root:
+            return []
+        if not root.left and not root.right:  # 如果是叶子节点
+            if expectNumber == root.val:
+                return [[root.val]]
+            else:
+                return []
+        a = self.FindPath(root.left, expectNumber-root.val) + self.FindPath(root.right, expectNumber-root.val)
+        return [[root.val] + i for i in a]
+
+
+pNode1 = TreeNode(10)
+pNode2 = TreeNode(5)
+pNode3 = TreeNode(12)
+pNode4 = TreeNode(4)
+pNode5 = TreeNode(7)
+pNode1.left = pNode2
+pNode1.right = pNode3
+pNode2.left = pNode4
+pNode2.right = pNode5
+s = Solution()
+print(s.FindPath(pNode1, 22))
