4da4f7ea8d66f438bf0f8fea3ec4c19287ae8bb2
songfish <songyu_727@163.com>
2019-07-31 11:27:51 +0800

    add solution

---
+++ 字符串的排列.py
@@ -0,0 +1,20 @@
+class Solution:
+    def Permutation(self, ss):
+        if not len(ss):
+            return []
+        if len(ss) == 1:
+            return list(ss)
+
+        charList = list(ss)
+        charList.sort()
+        pStr = []
+        for i in range(len(charList)):
+            if i > 0 and charList[i] == charList[i-1]:
+                continue
+            temp = self.Permutation(''.join(charList[:i])+''.join(charList[i+1:]))
+            for j in temp:
+                pStr.append(charList[i]+j)
+        return pStr
+
+s = Solution()
+print(s.Permutation('aa'))
