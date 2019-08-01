1bb207b7c3c833a212ddba58c47e15bc72fb5d3c
songfish <songyu_727@163.com>
2019-08-01 21:57:58 +0800

    add solution

--- 数组中出现次数超过一半的数字.py
+++ 数组中出现次数超过一半的数字.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 class Solution:
-    def MoreThanHalfNum_Solution(self, numbers):
+    def MoreThanHalfNum_Solution1(self, numbers):
         # write code here
         save_frequence = dict()
         for data in numbers:
@@ -8,11 +8,22 @@ class Solution:
                 save_frequence[data] = 1
             else:
                 save_frequence[data] += 1
-        length = len(numbers) / 2
+        length = len(numbers) / 2.0
         for i in save_frequence.values():
             if i > length:
                 result = list(save_frequence.keys())[list(save_frequence.values()).index(i)]
                 return result
+        return 0
+
+    def MoreThanHalfNum_Solution(self, numbers):
+        save_frequence = dict()
+        for data in numbers:
+            if data not in save_frequence:
+                save_frequence[data] = 1
+            else:
+                save_frequence[data] += 1
+            if save_frequence[data] > (len(numbers)/2.0):
+                return data
         return 0
 
 
